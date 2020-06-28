3b62e5d64f349d5c1799e12ebcdf1f1a89cfd6c3
chlwhdtn03 <chlwhdtn03@naver.com>
2020-06-28 19:43:57 +0900

    자신이 속한 그룹 표시

--- app/src/main/java/com/david0926/drop/Interface/DROPRetrofitInterface.java
+++ app/src/main/java/com/david0926/drop/Interface/DROPRetrofitInterface.java
@@ -8,12 +8,15 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
 import retrofit2.http.PartMap;
 
-public interface RetrofitRegisterInterface {
+public interface DROPRetrofitInterface {
 
     @Multipart
     @POST("v1/user")
@@ -27,6 +30,28 @@ public interface RetrofitRegisterInterface {
 
     @POST("v1/auth")
     Call<ResponseBody> Login(
+            @Body LoginModel lm
+    );
+
+    @POST("v1/post")
+    Call<ResponseBody> CreateGroup(
+            @Header("x-access-token") String token,
+            @Body String group
+    );
+
+    @POST("v1/group/join")
+    Call<ResponseBody> JoinGroup(
+            @Header("x-access-token") String token,
+            @Body String group
+    );
+
+    @GET("v1/group/user")
+    Call<ResponseBody> MyGroups(
+            @Header("x-access-token") String token
+    );
+
+    @POST("v1/auth")
+    Call<ResponseBody> getToken(
             @Body LoginModel lm
     );
 
